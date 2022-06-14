c8a51754c3e7e1bdd0aa9c20cf5aed7602bc23aa
Elder Lamarck <elder.lamarck@gmail.com>
2022-06-13 21:59:00 -0300

    Update UserController.java

--- aurum/src/main/java/com/cesar/projeto/sr2/aurum/controller/UserController.java
+++ aurum/src/main/java/com/cesar/projeto/sr2/aurum/controller/UserController.java
@@ -30,14 +30,12 @@ public class UserController {
 
     @PostMapping("/register")
     public String register(@ModelAttribute UserModel userModel){
-        System.out.println("register request: " + userModel);
         UserModel registeredUser = userService.registerUser(userModel.getLogin(), userModel.getPassword(), userModel.getEmail());
         return registeredUser == null ? "error_page" : "redirect:/login"; //if-else em uma linha
     }
 
     @PostMapping("/login")
     public String login(@ModelAttribute UserModel userModel, Model model){
-        System.out.println("login request: " + userModel);
         UserModel authenticated = userService.authenticate(userModel.getLogin(), userModel.getPassword());
         if(authenticated != null){
             model.addAttribute("userLogin", authenticated.getLogin());
